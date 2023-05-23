01375a72a3c788d96cc67fa745f3bddccd29012c
olivier-goy <olivier.goy.37@gmail.com>
2023-05-23 13:54:59 +0200

    commit for change branch

--- src/Pages/404/404.jsx
+++ src/Pages/404/404.jsx
@@ -1,5 +1,15 @@
+import "../404/404.css";
+
 function Error() {
-    return <h1>Error</h1>
+  return (
+    <div>
+      <h1 className="style404">404</h1>
+      <p className="styleTextError">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <a href="/" className="styleTextReturn">Retourner sur la page d'accueil</a>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
