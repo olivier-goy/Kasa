97bd78f87a2c8c373a4f12a494a02f4dfafe8c6d
olivier-goy <olivier.goy.37@gmail.com>
2023-05-24 14:21:13 +0200

    finished add header for all page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "../Header/Header.css";
+import logo from "/Users/olivier/Cours OpenClassroom/PROJET 6 REACT/Project/kasa/src/Assets/LOGO.png";
 
 function Header() {
   return (
     <div className="styleHeaderCommon">
-      <nav id="sideBar">
+      <img src={logo} alt="Logo de la sociète kasa, le logo est kasa en écriture et le premier a représente une maison"/>
+      <nav>
         <NavLink
           to="/"
           href="/"
