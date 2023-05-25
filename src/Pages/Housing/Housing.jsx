990ea78ea9aafe50bbfa6a1414cd9f0bec5dbb5b
olivier-goy <olivier.goy.37@gmail.com>
2023-05-25 23:12:04 +0200

    commit for end day

--- src/Pages/Housing/Housing.jsx
+++ src/Pages/Housing/Housing.jsx
@@ -1,10 +1,27 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { housingLists } from "../../datas/housingList.js";
 
 function Housing() {
 
+
     const { id } = useParams()
 
-    return <h1>housing { id }</h1>
+    return (
+        <div>
+            <div>
+                {housingLists.map((housingList, index) => {
+
+                    return housingList.id === id ? (
+
+                        housingList.pictures.map(housingPicture => {
+                            return <img key={index} src={housingPicture} alt={housingList.title} />
+                        })
+
+            ) : null
+                })}
+        </div>
+        </div >
+    )
 }
 
 export default Housing
