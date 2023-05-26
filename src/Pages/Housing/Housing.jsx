244b22a4f22928d8f9b6e11183563661e4870ca8
olivier-goy <olivier.goy.37@gmail.com>
2023-05-26 16:34:00 +0200

    add image housing and title

--- src/Pages/Housing/Housing.jsx
+++ src/Pages/Housing/Housing.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import { housingLists } from "../../datas/housingList.js";
-
 function Housing() {
 
 
@@ -10,16 +9,28 @@ function Housing() {
         <div>
             <div>
                 {housingLists.map((housingList, index) => {
+                    return (
+                        <div key={index}>
+                            {housingList.id === id ? (
+                                (
+                                    <div>
+                                        {housingList.pictures.map((housingPicture, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    <img src={housingPicture} alt={housingList.title} />
+                                                </div>
+                                            )
+                                        })}
+                                        <div><h1>{ housingList.title }</h1></div>
+                                    </div>
+                                )
 
-                    return housingList.id === id ? (
-
-                        housingList.pictures.map(housingPicture => {
-                            return <img key={index} src={housingPicture} alt={housingList.title} />
-                        })
-
-            ) : null
+                            ) : null}
+                        </div>
+                    )
                 })}
-        </div>
+            </div>
+
         </div >
     )
 }
