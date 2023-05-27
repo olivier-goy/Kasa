740ca31a34b194883cc3ff6fbfb1cf02dc6e4f30
olivier-goy <olivier.goy.37@gmail.com>
2023-05-27 12:22:10 +0200

    commit for change branch

---
+++ src/Components/Collapse/Collapse.jsx
@@ -0,0 +1,12 @@
+import "../Collapse/Collapse.css"
+
+function Collapse() {
+    return (
+        <div className="styleCollapse">
+            <div>Text</div>
+            <div>X</div>
+        </div>
+    )
+}
+
+export default Collapse
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,13 @@
+import Collapse from "../../Components/Collapse/Collapse"
+
+
 function About() {
-    return <h1>about</h1>
+    return (
+        <div>
+            <h1> about</h1>
+            <Collapse/>
+        </div>
+        )
 }
 
 export default About
