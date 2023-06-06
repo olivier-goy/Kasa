6fdc96a4d2e84133ad7e946144683909c7821e47
olivier-goy <olivier.goy.37@gmail.com>
2023-06-06 18:15:08 +0200

    add host item

---
+++ src/Components/Host/Host.jsx
@@ -0,0 +1,12 @@
+import "../Host/Host.css"
+
+function Host({ host, picture }) {
+    return (
+        <div className="hostStyle">
+                <p>{host.name}</p>
+                <img src={picture.picture} alt="profil" />
+        </div>
+    )
+}
+
+export default Host
--- src/Pages/Housing/Housing.jsx
+++ src/Pages/Housing/Housing.jsx
@@ -1,4 +1,5 @@
 import Carousel from "../../Components/Carousel/Carousel"
+import Host from "../../Components/Host/Host"
 import Rating from "../../Components/Rating/Rating"
 import "../Housing/Housing.css"
 
@@ -15,7 +16,12 @@ function Housing() {
                 "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
                 "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
                 "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"]} />
-            <div className="ratingHost">
+            <div className="hostHousing">
+                <Host
+                    host={{ "name": "Sqsd cqeffeadf" }}
+                    picture={{ "picture": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg" }} />
+            </div>
+            <div className="ratingHousing">
                 <Rating rating="4" />
             </div>
         </div>
