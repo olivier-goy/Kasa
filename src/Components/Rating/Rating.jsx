715ad52f70b7046cd285097eb1a3e9ecd850c67c
olivier-goy <olivier.goy.37@gmail.com>
2023-06-06 11:00:50 +0200

    add logo rating light and off more import logo in rating.jsxw

--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -1,8 +1,14 @@
 import "../Rating/Rating.css"
+import RatingLight from "../../Assets/RatingLight.png"
+import RatingOff from "../../Assets/RatingOff.png"
 
 function Rating() {
     return (
-        <h1>teste rating</h1>
+        <div>
+            <h1>teste rating</h1>
+            <img src={RatingLight} alt="" />
+            <img src={RatingOff} alt="" />
+        </div>
     )
 }
 
